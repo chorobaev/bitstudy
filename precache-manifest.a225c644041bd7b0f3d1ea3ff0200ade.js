14e136f6a3cd277ae2f907a67a028b12957b5ed2
chorobaev <chorobaev.nurbol@gmail.com>
2019-12-15 00:16:16 +0600

    Updates

---
+++ precache-manifest.a225c644041bd7b0f3d1ea3ff0200ade.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25803dadec75121fa6908d4c4beca27",
+    "url": "/bitstudy/index.html"
+  },
+  {
+    "revision": "b0825fc4477446f38f8d",
+    "url": "/bitstudy/static/css/main.ab102a0b.chunk.css"
+  },
+  {
+    "revision": "e7954ef29b817987d1a1",
+    "url": "/bitstudy/static/js/2.066e8327.chunk.js"
+  },
+  {
+    "revision": "a290a4b60f8d5284bd7ab92bebea836f",
+    "url": "/bitstudy/static/js/2.066e8327.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0825fc4477446f38f8d",
+    "url": "/bitstudy/static/js/main.b69de8aa.chunk.js"
+  },
+  {
+    "revision": "026bcb4f3abcef0a47d0",
+    "url": "/bitstudy/static/js/runtime-main.bd3dc3af.js"
+  },
+  {
+    "revision": "311692cf235516cc0424bfd0b2f1c2ae",
+    "url": "/bitstudy/static/media/car.311692cf.png"
+  },
+  {
+    "revision": "0babc35260f3f8ffff1b1c00ec1447d2",
+    "url": "/bitstudy/static/media/logo-big.0babc352.png"
+  }
+]);
